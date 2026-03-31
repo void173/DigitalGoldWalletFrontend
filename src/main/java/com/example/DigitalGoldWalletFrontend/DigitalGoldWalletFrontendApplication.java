4efdb45f574aad61a70cb405b654f92938ae5f2f
Prathmesh_H <handeprathmesh1@gmail.com>
2026-03-31 14:52:36 +0530

    initial commit

---
+++ src/main/java/com/example/DigitalGoldWalletFrontend/DigitalGoldWalletFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DigitalGoldWalletFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGoldWalletFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalGoldWalletFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DigitalGoldWalletFrontend/DigitalGoldWalletFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DigitalGoldWalletFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalGoldWalletFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
